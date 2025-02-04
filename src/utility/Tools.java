1ef46049388b59c48fa0c57af3813dbef2fc313d
DrPacar <lukapacar11@gmail.com>
2025-02-04 22:21:27 +0100

    Adding Point and Map Utilities

--- src/utility/Tools.java
+++ src/utility/Tools.java
@@ -9,8 +9,161 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Tools {
+
+    /** Represents a Point in a 2D Space (using long as coordinates)*/
+    public static class Point2D {
+        int x;
+        int y;
+
+        public Point2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Getters
+        public int getX() {return x;}
+        public int getY() {return y;}
+
+        // Setters
+        public void setX(int x) {this.x = x;}
+        public void setY(int y) {this.y = y;}
+
+        // Hash
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Point2D that = (Point2D) o;
+            return x == that.x && y == that.y;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        // toString
+
+        @Override
+        public String toString() {
+            return "Point(" + x + ", " + y + ")";
+        }
+    }
+
+    /** Represents a Point in a 2D Space (using long as coordinates)*/
+    public static class Point2D_Long {
+        long x;
+        long y;
+
+        public Point2D_Long(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Getters
+        public long getX() {return x;}
+        public long getY() {return y;}
+
+        // Setters
+        public void setX(long x) {this.x = x;}
+        public void setY(long y) {this.y = y;}
+
+        // Hash
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Point2D_Long that = (Point2D_Long) o;
+            return x == that.x && y == that.y;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        // toString
+
+        @Override
+        public String toString() {
+            return "Point(" + x + ", " + y + ")";
+        }
+    }
+
+    /** Represents a Point in a 2D Space (using double as coordinates)*/
+    public static class Point2D_Double {
+        double x;
+        double y;
+
+        public Point2D_Double(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Getters
+        public double getX() {return x;}
+        public double getY() {return y;}
+
+        // Setters
+        public void setX(double x) {this.x = x;}
+        public void setY(double y) {this.y = y;}
+
+        // Hash
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Point2D_Double that = (Point2D_Double) o;
+            return Double.compare(x, that.x) == 0 && Double.compare(y, that.y) == 0;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        // toString
+
+        @Override
+        public String toString() {
+            return "Point(" + x + ", " + y + ")";
+        }
+    }
+
+
+    // Maps
+
+    /**
+     * Generates a Character Map from the given Input
+     * @param input The Input
+     * @return The Map
+     */
+    public static List<List<Character>> generateMap(List<String> input) {
+        List<List<Character>> map = new ArrayList<>();
+        for (String line : input) {
+            List<Character> row = new ArrayList<>();
+            for (char c : line.toCharArray()) {
+                row.add(c);
+            }
+            map.add(row);
+        }
+        return map;
+    }
+
+    /**
+     * Prints a 2D-Map
+     * @param map The Map to print
+     */
+    public static void printMap(List<List<Character>> map) {
+        for (List<?> row : map) {
+            for (Object c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    // Executing Day-Puzzles
     /**
      * Returns all the Lines of the parsed File
      * @param fileName The Path to the File
@@ -86,4 +239,6 @@ public class Tools {
             throw new IllegalArgumentException("Seems like there was an Implementation Error : " + className, e);
         }
     }
+
+
 }
